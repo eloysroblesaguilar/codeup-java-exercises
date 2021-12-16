9a5abf689ca052b3abcedeef99e65d08618da340
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2021-12-16 09:15:16 -0600

    warmup

--- src/Warmups.java
+++ src/Warmups.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Warmups {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Java is statically typed, while JavaScript is dynamically typed.");
+
+        System.out.println("What did you eat for breakfast today?");
+        String userBreakfast = scanner.nextLine();
+        System.out.printf("%s? Sounds pretty tasty... i guess", userBreakfast);
+
+
     }
 }
