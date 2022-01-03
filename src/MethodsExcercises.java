dee0e7e78c1045d124b8e93f5ce98d70fb5cf4d1
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2022-01-03 16:43:45 -0600

    completed the basic arithmetic portion of the methods exercises

---
+++ src/MethodsExcercises.java
@@ -0,0 +1,36 @@
+public class MethodsExcercises {
+//    !. Basic Arithmetic
+    public static int add (int num1, int num2) {
+        return num1 + num2;
+    }
+
+    public static int subtract (int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int multiply (int num1, int num2) {
+        return num1 - num2;
+    }
+
+    public static int divide (int num1, int num2) {
+        return num1 / num2;
+
+    }
+
+    public static int modulus ( int num1, int num2) {
+        return num1 % num2;
+    }
+
+//    2.
+
+
+
+
+    public static void main(String[] args) {
+        System.out.println(add(2,2));
+        System.out.println(subtract(2,2));
+        System.out.println(multiply(2,2));
+        System.out.println(divide(2,2));
+        System.out.println(modulus(2,2));
+    }
+ }
