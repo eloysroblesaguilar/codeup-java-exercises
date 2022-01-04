126a9a7d8654aaf2a3001d8d0ea68b7c1b87cb3a
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2022-01-04 09:55:22 -0600

    completed the second section of the methods exercise

--- src/MethodsExcercises.java
+++ src/MethodsExcercises.java
@@ -1,54 +1,76 @@
 import java.util.Scanner;
 
 public class MethodsExcercises {
-//    !. Basic Arithmetic
-    public static int add (int num1, int num2) {
+    //    !. Basic Arithmetic
+    public static int add(int num1, int num2) {
         return num1 + num2;
     }
 
-    public static int subtract (int num1, int num2) {
+    public static int subtract(int num1, int num2) {
         return num1 - num2;
     }
 
-    public static int multiply (int num1, int num2) {
+    public static int multiply(int num1, int num2) {
         return num1 - num2;
     }
 
-    public static int divide (int num1, int num2) {
+    public static int divide(int num1, int num2) {
         return num1 / num2;
 
     }
 
-    public static int modulus ( int num1, int num2) {
+    public static int modulus(int num1, int num2) {
         return num1 % num2;
     }
 
-////    2.
-//    public static int getInteger ( int min, int max) {
+    //    2.
+    public static void getInteger(float min, float max) {
+        Scanner scan = new Scanner(System.in);
+        Boolean yes;
+
+        System.out.printf("Please enter in an integer between %.0f and %.0f %n", min, max);
+        float userInt = scan.nextInt();
+        if (max >= userInt && userInt >= min) {
+            System.out.printf("%.0f is a valid answer, Thank You!%n", userInt);
+            System.out.println("WOULD YOU LIKE TO CONTNIUE? \nTRUE FOR YES |  FALSE FOR NO ");
+            yes = scan.nextBoolean();
+            if (yes == true) {
+                getInteger(min, max);
+            }
+        } else {
+            System.out.printf("%.0f is not a valid answer!%n", userInt);
+            System.out.println("WOULD YOU LIKE TO CONTNIUE? \nTRUE FOR YES |  FALSE FOR NO ");
+            yes = scan.nextBoolean();
+            if (yes == true) {
+                getInteger(min, max);
+            }
+        }
+
+    }
+
+
+
+
+
 //
-//    }
-//    Boolean rollTheDice;
-//    public static void diceRolling () {
+//    public static void diceRolling() {
 //        Scanner sc = new Scanner(System.in);
+//        Boolean rollTheDice;
+//
+//        System.out.println("Hello!");
 //        System.out.println("Please enter the number of sides for the pair of dice");
-//        int sides = sc.nextInt();
-//        do {
-//            System.out.println("Would you like to commence the rolling?");
-//             rollTheDice = sc.nextBoolean();
+//        float sides = sc.nextInt();
+//        System.out.printf("Your dice will have %.0f sides!%n", sides);
+//
+//        System.out.println("Would you like to commence the rolling?");
+//        rollTheDice = sc.nextBoolean();
 //
 //
-//        } while (rollTheDice);
 //    }
-
-
-
 
 
     public static void main(String[] args) {
-        System.out.println(add(2,2));
-        System.out.println(subtract(2,2));
-        System.out.println(multiply(2,2));
-        System.out.println(divide(2,2));
-        System.out.println(modulus(2,2));
+        getInteger(1, 40);
+
     }
- }
+}
