b04c332e88c720a147fb318e47dcfc76e73e03ee
Eloys Robles Aguilar <roblesaguilar.eloys@gmail.com>
2022-01-07 09:31:12 -0600

    went back and cleaned up array basics before I move onto completing the movie list section of the arrays exercise

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -3,36 +3,31 @@ import java.util.Arrays;
 
 public class ArraysExercises {
 
-//    1.
+    //    1.
     public static Person[] addPerson(Person[] people, Person addedPerson) {
-
 
         Person[] newPeople = new Person[(people.length + 1)];
 
-        for (int i = 0; i < people.length + 1; i++) {
-            newPeople[i] = people[i];
-
-            if (i == people.length + 1) {
+        for (int i = 0; i <= people.length; i++) {
+            if (i == people.length) {
                 newPeople[i] = addedPerson;
+            } else {
+                newPeople[i] = people[i];
             }
-
+            System.out.println(newPeople[i].getName());
         }
-
-
-        System.out.println(Arrays.toString(newPeople));
-
         return newPeople;
     }
 
 
     public static void main(String[] args) {
         Person[] people = {new Person("Eloys"), new Person("Eloys II"), new Person("Eloys III")};
-        for (Person person : people){
+        for (Person person : people) {
             System.out.println(person.getName());
         }
         Person person4 = new Person("Eloys IV");
-
-//        addPerson(people, person4);
+//        System.out.println(people.length);
+        addPerson(people, person4);
 
     }
 }
--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,7 +1,39 @@
 package movies;
 
+import util.Input;
+
 public class MoviesApplication {
+
+
+    public static void movieFetch() {
+        Input in = new Input();
+        System.out.println("What would you like to do?");
+
+        System.out.println("0 - exit\n1 - view all movies\n 2 -view movies in the animated category\n3 - view movies in the drama category\n4 - view movies in the horror category\n5 - view movies in the sci-fi category");
+        System.out.printf("Enter your choice : ");
+       int userResponse = in.getInt();
+
+       switch (userResponse){
+           case 0:
+               return;
+           case 1:
+               MoviesArray.findAll();
+               break;
+           case 2:
+               break;
+           case 3:
+               break;
+           case 4:
+               break;
+           case 5:
+               break;
+       }
+
+    }
+
     public static void main(String[] args) {
+
+
 
     }
 }
